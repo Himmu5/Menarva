2927bb38828f4b9024df317a09de30e71e96f17d
Himanshu chauhan <Adityachauhan5566@gmail.com>
2023-09-15 08:28:08 +0530

    Router added

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <div className='font-Inter'>
-      <App />
-    </div>
+    <BrowserRouter>
+      <div className='font-Inter'>
+        <App />
+      </div>
+    </BrowserRouter>
   </React.StrictMode>,
 )
