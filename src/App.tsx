1f675423590bf7a2cb97097e5e2a9da91e5043d8
Himanshu chauhan <Adityachauhan5566@gmail.com>
2023-09-18 08:39:07 +0530

    Calendar Added

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Navbar from "./Components/Common/Navbar"
 import SignIn from "./Components/SignIn/SignIn"
 import Home from "./Components/SOP/Home"
 import Stores from "./Components/Store/Stores"
+import StoreCalendar from "./Components/Store/Calendar"
 
 function App() {
 
@@ -14,7 +15,7 @@ function App() {
         <Route path="signin" element={<SignIn />} />
         <Route path="/" element={<Home />} />
         <Route path="/stores" element={<Stores />} />
-        <Route path="/calender" element={<Stores />} />
+        <Route path="/calendar" element={<StoreCalendar />} />
       </Routes>
 
 
--- src/Components/Store/Calendar.tsx
+++ src/Components/Store/Calendar.tsx
@@ -1,11 +1,38 @@
-import {FC} from 'react'
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+import { FaAngleLeft } from 'react-icons/fa'
+import { Link } from 'react-router-dom';
 
 
-type P = object
+function CalendarWithHighlights() {
 
-const Calendar:FC<P> =()=>{
-  return <div>
-      
-</div>
+
+  const highlightDates: Date[] = [
+    new Date('2023-09-01'),
+    new Date('2023-09-06'),
+    new Date('2023-09-10')
+    // Add more highlighted dates here
+  ];
+
+  const isDateHighlighted = (date: Date) =>
+    highlightDates.some((highlightedDate) =>
+      date.toDateString() === highlightedDate.toDateString()
+    );
+
+  return (
+    <div className='max-w-7xl mx-auto flex flex-col'>
+      <Link to={'/'} className='w-fit'><FaAngleLeft size={25} className=" w-fit text-xl mt-4 " /></Link>
+      <div className='flex items-center justify-center min-h-[80vh]'>
+
+        <Calendar
+          className={"max-w-sm"}
+          tileClassName={({ date }) => {
+            return isDateHighlighted(date) ? 'h-12 rounded-full bg-green-500' : '';
+          }}
+        />
+      </div>
+    </div>
+  );
 }
-export default Calendar;
+
+export default CalendarWithHighlights;
