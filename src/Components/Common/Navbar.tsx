8e0cd357bd5140ee2831ba0f59472e980c152d39
Himanshu chauhan <Adityachauhan5566@gmail.com>
2023-09-22 10:24:49 +0530

    Stores option added

--- src/Components/Common/Navbar.tsx
+++ src/Components/Common/Navbar.tsx
@@ -1,11 +1,12 @@
 import { FC } from 'react'
 import { Logo } from '../../../public/index'
+import { Link } from 'react-router-dom'
 
 type P = object
 
 const Navbar: FC<P> = () => {
-    return <div className=' max-w-7xl mx-auto border-b '>
-        <img src={Logo} alt='Logo' className='ml-1 mt-3' />
+    return <div className=' max-w-7xl mx-auto border-b flex  '>
+        <Link to={"/"}><img src={Logo} alt='Logo' className='ml-1 mt-3' /></Link>
     </div>
 }
 export default Navbar;
--- src/Components/Store/StoreOptions.tsx
+++ src/Components/Store/StoreOptions.tsx
@@ -1,14 +1,21 @@
-import {FC} from 'react'
+import { FC } from 'react'
 
 type P = object
 
-const StoreOptions:FC<P> =()=>{
-  return <div className='flex flex-col '>
-    {  [...Array(3).keys()].map(()=>{
-        return <div>
+const StoreOptions: FC<P> = () => {
 
-        </div>
-    })}      
-</div>
+  const data = [
+    "Ministores",
+    "Inventory",
+    "SOPS"
+  ]
+
+  return <div className='flex flex-col max-w-7xl mx-auto m-3 space-y-3 '>
+    {data.map((option) => {
+      return <div className='px-3 py-2 rounded-md border shadow-lg hover:scale-105'>
+        {option}
+      </div>
+    })}
+  </div>
 }
 export default StoreOptions;
