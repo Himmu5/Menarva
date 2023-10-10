4296e7d97127934b767225275469bae068ffdb86
Himanshu chauhan <Adityachauhan5566@gmail.com>
2023-10-10 08:06:48 +0530

    Image show component added

--- src/Sales/ShowSalesReport.tsx
+++ src/Sales/ShowSalesReport.tsx
@@ -1,23 +1,36 @@
-import { FC } from 'react'
+import { FC, useState } from 'react'
 import { withShop } from '../HOC/withShop'
 import { Sales } from '../Typings/Shop'
 import { Link } from 'react-router-dom'
 import Error from '../Components/Error/404Page'
+import Dialog from '@mui/material/Dialog';
+import { Button } from '@mui/material'
+
 type P = {
     dailySales: { result: Sales, message: string }
 }
 const ShowSalesReport: FC<P> = ({ dailySales }) => {
-    // console.log("daily sales : ", dailySales);
+    
+    const [ open , setOpen ] = useState(false);
+
     if (dailySales.result === undefined) {
         return <Error message={dailySales.message} />
     }
+
+    
     return <div className='min-h-[80vh] max-w-5xl mx-auto flex justify-center items-center '>
 
         <div className='min-h-[20vh] w-1/3 text-center space-y-5 py-5 border m-4 shadow-md rounded-md flex flex-col p-2  '>
             <h1 className='text-xl font-bold '>Sales Datails</h1>
             <p><span className='  font-bold text-lg'> Date :</span> {dailySales?.result.date.toLocaleString()}</p>
             <p><span className='font-bold text-lg'>Total Sales :</span> {dailySales?.result.totalSales}</p>
-            {dailySales?.result?.imageUrl ? <img src={dailySales?.result?.imageUrl} alt="sales image" /> : "No Sales Image Found"}
+            {dailySales?.result?.imageUrl ? <img src={dailySales?.result?.imageUrl} alt="sales image" onClick={()=>setOpen(!open)} /> : "No Sales Image Found"}
+
+            <Dialog open={open} onClose={() => { setOpen(!open)}} className=' relative flex flex-col  ' >
+                <img src={dailySales?.result?.imageUrl!} alt="sales image" />
+                <Button onClick={()=>setOpen(!open)} children="OK" variant='contained' className='w-fit self-center bottom-4  ' sx={{ position : 'absolute'  }} />
+            </Dialog>
+
             <Link to={"/"} className='px-3 py-1 bg-indigo-400 rounded-md shadow-md text-white my-3 hover:scale-95 ' >back to Shops</Link>
         </div>
 
