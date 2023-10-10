dd4f4fe30520209402cb543f6da2c60ffba27ccf
Himanshu chauhan <Adityachauhan5566@gmail.com>
2023-10-10 14:05:37 +0530

    Minor fixes

--- src/Components/UI-Components/ApprovalStatus.tsx
+++ src/Components/UI-Components/ApprovalStatus.tsx
@@ -1,12 +1,10 @@
 import { FC } from 'react'
-import { Sales } from '../../Typings/Shop'
 type P = {
-    dailySales: { result: Sales, message: string }
+    status: number;
+    message:string
 }
-const ApprovalStatus: FC<P> = ({ dailySales }) => {
+const ApprovalStatus: FC<P> = ({ status , message }) => {
 
-    // console.log("daily Sales : ", dailySales.result.ownerConsentStatus);
-    const status = dailySales.result.ownerConsentStatus;
     let extraClass = " ";
     if (status === 1) {
         extraClass = " text-green-400 border-green-400 "
@@ -17,8 +15,8 @@ const ApprovalStatus: FC<P> = ({ dailySales }) => {
         extraClass = " text-red-400  border-red-400 "
     }
 
-    return <div className='text-sm items-center gap-1 flex self-start '>
-        <p className='font-bold'>Owner acceptance status : </p> <p className={' px-2 py-1 rounded-md shadow-md border-2 '+extraClass}> {status === 1 ? " Accepted " : status === 2 ? " Pending " : status === 3 && " Rejected"} </p>
+    return <div className='text-sm items-center gap-1 flex self-start text-left '>
+        <p className='font-bold min-w-[35vh] '>{message} : </p> <p className={' px-2 py-1 rounded-md shadow-md border-2 '+extraClass}> {status === 1 ? " Accepted " : status === 2 ? " Pending " : status === 3 && " Rejected"} </p>
     </div>
 }
 export default ApprovalStatus;
