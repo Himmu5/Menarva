9c84022aec8e5dde2ed3db12f747c8cbd7b83b03
Himanshu chauhan <Adityachauhan5566@gmail.com>
2023-10-10 13:51:00 +0530

    minor chnage

--- src/Axios/Sop.ts
+++ src/Axios/Sop.ts
@@ -1,9 +1,10 @@
-import axiosInstance from "./axios";
+import axios from "axios";
+// import axiosInstance from "./axios";
 
 export const getSOP = () => {
 //   const token = "Bearer " + localStorage.getItem("token");
-  return axiosInstance
-    .get("/api/v1/shops/3/sops")
+  return axios
+    .get("http://localhost:8080/api/v1/shops/3/sops" , { headers: { Authorization: 'Bearer ' + localStorage.getItem('token') } })
     .then((res) => {
       console.log(res.data);
     })
