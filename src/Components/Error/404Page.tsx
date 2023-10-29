e20e6051c23dcfc1ada22b6f3439d68e89098b61
Himanshu chauhan <Adityachauhan5566@gmail.com>
2023-10-29 11:00:18 +0530

    Back button added in Error message Conponent

--- src/Components/Error/404Page.tsx
+++ src/Components/Error/404Page.tsx
@@ -1,12 +1,19 @@
 import { FC } from 'react'
+import { useNavigate } from 'react-router-dom';
+import { Button } from '@mui/material';
+
 type P = {
     message?: string;
 }
 const Error: FC<P> = ({ message }) => {
+    const Navigate = useNavigate();
 
     return <div className='h-[80vh] w-full flex justify-center items-center text-center text-sm'>
-        <div className=' font-bold text-xl '>
-            {message}
+        <div className='flex flex-col items-center '>
+            <div className=' font-bold text-xl '>
+                {message}
+            </div>
+            <div className='w-fit px-3  text-white '  onClick={() => Navigate(-1)}><Button variant='contained' children="back" /></div>
         </div>
     </div>
 }
