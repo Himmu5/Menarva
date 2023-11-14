4613cd138f52885c26f17ba70c4766d32ebbf36a
korade-krushna <101387736+korade-krushna@users.noreply.github.com>
2023-11-14 16:55:40 +0530

    removed ngrok header

--- src/Axios/Headers.ts
+++ src/Axios/Headers.ts
@@ -1,16 +1,14 @@
 export const CustomHeader = {
-    Authorization: "Bearer " + localStorage.getItem("token"),
-    "ngrok-skip-browser-warning": 69420,
-    // dummyhost: "Chroma.Reliance.minerva.com"
-  }
-  
-  export const OwnerHeader = {
-    // Authorization: "Bearer " + localStorage.getItem("token"),
-    "ngrok-skip-browser-warning": 69420,
-    // dummyhost: "minerva.com"
-  }
-  
-  
-  
-  // ,
-  // Entity : "Chroma"
+  Authorization: "Bearer " + localStorage.getItem("token"),
+  // "ngrok-skip-browser-warning": 69420,
+  // dummyhost: "Chroma.Reliance.minerva.com"
+};
+
+export const OwnerHeader = {
+  // Authorization: "Bearer " + localStorage.getItem("token"),
+  // "ngrok-skip-browser-warning": 69420,
+  // dummyhost: "minerva.com"
+};
+
+// ,
+// Entity : "Chroma"
